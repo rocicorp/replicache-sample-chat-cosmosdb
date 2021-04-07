230164769ca78d0712acfef60d8effe36f7c5714
Aaron Boodman <aaron@aaronboodman.com>
2021-04-06 15:01:30 -1000

    Follow CosmosDB setup instructions

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
-import pgInit from 'pg-promise';
+import {CosmosClient} from "@azure/cosmos";
 
-export async function getDB() {
-  const pgp = pgInit();
-  const db = pgp({
-    connectionString: process.env.REPLICHAT_DB_CONNECTION_STRING,
+const endpoint = mustEnv("REPLICHAT_COSMOSDB_ENDPOINT");
+const key = mustEnv("REPLICHAT_COSMOSDB_KEY");
+const databaseID = mustEnv("REPLICHAT_COSMOSDB_DATABASE_ID");
+const containerID = mustEnv("REPLICHAT_COSMOSDB_CONTAINER_ID");
+const partitionKey = "/partitionKey";
+
+export async function initDB() {
+  const client = new CosmosClient({ endpoint, key });
+  await client.databases.createIfNotExists({
+    id: databaseID
   });
-  await db.connect();
-  return db;
+  await client.database(databaseID).containers.createIfNotExists({
+    id: containerID,
+    partitionKey
+  });
+}
+
+function mustEnv(name) {
+  const r = process.env[name];
+  if (!r) {
+    throw new Error(`Required environment variable ${name} not found`);
+  }
+  return r;
 }
